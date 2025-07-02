b7fc9ec5872a1ef838243cc296f02beb04b87053
Carron cedric <carron.cedric@laposte.net>
2025-07-02 09:09:38 +0200

    fixed cors

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var cartsRouter = require('./routes/carts')
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var tripsRouter = require("./routes/trips");
+const cors = require("cors");
 var app = express();
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
